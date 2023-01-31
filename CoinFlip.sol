20ce62e0a9b269d08885071ae151494ad3b8360a
vcth4nh <tvu25337@gmail.coam>
2023-02-01 02:00:59 +0700

    CoinFlip test 1

--- CoinFlip.sol
+++ CoinFlip.sol
@@ -2,35 +2,37 @@
 pragma solidity ^0.8.0;
 
 contract CoinFlip {
+	uint256 public consecutiveWins;
+	uint256 lastHash;
+	uint256 FACTOR =
+		57896044618658097711785492504343953926634992332820282019728792003956564819968;
+	CoinFlip cf;
 
-  uint256 public consecutiveWins;
-  uint256 lastHash;
-  uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+	constructor(address _target) {
+		consecutiveWins = 0;
+		cf = CoinFlip(_target);
+	}
 
-  
-  constructor(address _target) {
-    consecutiveWins = 0;
-    CoinFlip _target;
-  }
+	function flip(bool _guess) public returns (bool) {
+		uint256 blockValue = uint256(blockhash(block.number - 1));
 
-  function flip(bool _guess) public returns (bool) {
-    uint256 blockValue = uint256(blockhash(block.number - 1));
+		if (lastHash == blockValue) {
+			revert();
+		}
 
-    if (lastHash == blockValue) {
-      revert();
-    }
+		lastHash = blockValue;
+		uint256 coinFlip = blockValue / FACTOR;
 
-    lastHash = blockValue;
-    uint256 coinFlip = blockValue / FACTOR;
-    
-    bool side = coinFlip == 1 ? true : false;
+		bool side = coinFlip == 1 ? true : false;
 
-    if (side == _guess) {
-      consecutiveWins++;
-      return true;
-    } else {
-      consecutiveWins = 0;
-      return false;
-    }
-  }
+		require(cf.flip(side), "error");
+
+		if (side == _guess) {
+			consecutiveWins++;
+			return true;
+		} else {
+			consecutiveWins = 0;
+			return false;
+		}
+	}
 }
